c19214d6b70e7d75ab5afd5436f63e97bef471f2
ifer-itcast <yangkangkang@itcast.cn>
2021-07-29 22:50:08 +0800

    .

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,13 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
-  }
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template: './public/index.html'
+  })]
 };
