948a7ab6541da7240ad66439deabc15c259e1201
ifer-itcast <yangkangkang@itcast.cn>
2021-08-01 19:00:15 +0800

    .

--- src/App.vue
+++ src/App.vue
@@ -1,80 +1,22 @@
 <template>
-  <section class="todoapp">
-    <!-- 除了驼峰, 还可以使用-转换链接 -->
-    <TodoHeader :arr="list" @create="createFn"></TodoHeader>
-    <TodoMain :arr="showArr" @del="deleteFn"></TodoMain>
-    <TodoFooter
-      :farr="showArr"
-      @changeType="typeFn"
-      @clear="clearFn"
-    ></TodoFooter>
-  </section>
+  <div>
+    <button @click="show = !show">销毁</button>
+    <hr>
+    <Life v-if="show"/>
+  </div>
 </template>
 
 <script>
-// 1.0 样式引入
-import './styles/base.css';
-import './styles/index.css';
-
-import TodoHeader from './components/TodoHeader';
-import TodoMain from './components/TodoMain';
-import TodoFooter from './components/TodoFooter';
-
+import Life from './components/Life.vue'
 export default {
-  components: {
-    TodoHeader,
-    TodoMain,
-    TodoFooter
-  },
+  name: 'App',
   data() {
     return {
-      list: JSON.parse(localStorage.getItem('TODO') || '[]'),
-      getSel: 'all'
-    };
-  },
-  methods: {
-    createFn(taskName) {
-      const id =
-        this.list.length === 0 ? 100 : this.list[this.list.length - 1].id + 1;
-      this.list.push({
-        id,
-        name: taskName,
-        isDone: false
-      });
-    },
-    deleteFn(id) {
-      const idx = this.list.findIndex((obj) => obj.id === id);
-      this.list.splice(idx, 1);
-    },
-    typeFn(str) {
-      this.getSel = str;
-    },
-    clearFn() {
-      // 清空已完成 => 保留未完成
-      this.list = this.list.filter((obj) => !obj.isDone);
+      show: true
     }
   },
-  computed: {
-    showArr() {
-      if (this.getSel === 'yes') {
-        return this.list.filter((obj) => obj.isDone === true);
-      }
-
-      if (this.getSel === 'no') {
-        return this.list.filter((obj) => obj.isDone === false);
-      }
-
-      return this.list;
-    }
-  },
-  watch: {
-    list: {
-      handler() {
-        localStorage.setItem('TODO', JSON.stringify(this.list));
-      },
-      // 改变数组中对象里面的数据时会发挥作用
-      deep: true
-    }
+  components: {
+    Life
   }
-};
+}
 </script>
---
+++ src/components/Life.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <div id="con">
+      {{ msg }}
+    </div>
+    <button @click="msg = 'xxx'">update</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Life',
+
+  data() {
+    return {
+      msg: 'Hello Vue'
+    };
+  },
+  beforeCreate() {
+    // !#1，拿不到 data 和 methods 中的数据
+    console.log('beforeCreate');
+  },
+  created() {
+    // !#2
+    console.log('created');
+  },
+  beforeMount() {
+    // !#3
+    console.log('beforeMount', document.querySelector('#con'));
+  },
+  mounted() {
+    // !#4
+    console.log('mounted', document.querySelector('#con'));
+  },
+  beforeUpdate() {
+    // !#5
+    console.log('beforeUpdate', document.querySelector('#con').innerHTML);
+  },
+  updated() {
+    // !#6
+    console.log('updated', document.querySelector('#con').innerHTML);
+  },
+  beforeDestroy() {
+    // !#7
+    console.log('beforeDestroy');
+  },
+  destroyed() {
+    // !#8
+    console.log('destroyed');
+  }
+};
+</script>
