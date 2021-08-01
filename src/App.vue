fa88457360296ce20fdd7ae274fdd5098a1ce803
ifer-itcast <yangkangkang@itcast.cn>
2021-08-01 16:18:31 +0800

    .

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,47 @@
 <template>
   <div>
-    <!-- 
-      目标: 父(App.vue) -> 子(MyProduct.vue) 分别传值进入
-      需求: 每次组件显示不同的数据信息
-      步骤(口诀):
-        1. 子组件 - props - 变量 (准备接收)
-        2. 父组件 - 传值进去
-     -->
-    <Product
-      title="好吃的口水鸡"
-      price="50"
-      intro="开业大酬宾, 全场8折"
-    ></Product>
-    <Product
-      title="好可爱的可爱多"
-      price="20"
-      intro="老板不在家, 全场1折"
-    ></Product>
-    <Product title="好贵的北京烤鸭" price="290" :intro="str"></Product>
+    <MyProduct
+      v-for="obj in list"
+      :key="obj.id"
+      :title="obj.proname"
+      :price="obj.proprice"
+      :intro="obj.info"
+    ></MyProduct>
   </div>
 </template>
 
 <script>
-import Product from './components/MyProduct';
+// 目标: 循环使用组件-分别传入数据
+import MyProduct from './components/MyProduct';
 export default {
   data() {
     return {
-      str: '好贵啊, 快来啊, 好吃'
+      list: [
+        {
+          id: 1,
+          proname: '超级好吃的棒棒糖',
+          proprice: 18.8,
+          info: '开业大酬宾, 全场8折'
+        },
+        {
+          id: 2,
+          proname: '超级好吃的大鸡腿',
+          proprice: 34.2,
+          info: '好吃不腻, 快来买啊'
+        },
+        {
+          id: 3,
+          proname: '超级无敌的冰激凌',
+          proprice: 14.2,
+          info: '炎热的夏天, 来个冰激凌了'
+        }
+      ]
     };
   },
   // 3. 注册组件
   components: {
-    // Product: Product // key和value变量名同名 - 简写
-    Product
+    // MyProduct: MyProduct
+    MyProduct
   }
 };
 </script>
-
-<style></style>
