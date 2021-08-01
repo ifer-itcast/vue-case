2a5936cb56085f29708dc4fd5c39c08aeb329a78
ifer-itcast <yangkangkang@itcast.cn>
2021-08-01 16:41:55 +0800

    .

--- src/App.vue
+++ src/App.vue
@@ -1,63 +1,26 @@
 <template>
-  <div style="overflow: hidden;">
-    <div style="float: left;">
-      <MyProduct
-        v-for="(obj, ind) in list"
-        :key="obj.id"
-        :title="obj.proname"
-        :price="obj.proprice"
-        :intro="obj.info"
-        :index="ind"
-        @subprice="fn"
-      ></MyProduct>
-    </div>
-    <div style="float: left;">
-      <List :arr="list" />
-    </div>
-  </div>
+  <section class="todoapp">
+    <!-- 除了驼峰, 还可以使用-转换链接 -->
+    <TodoHeader></TodoHeader>
+    <TodoMain></TodoMain>
+    <TodoFooter></TodoFooter>
+  </section>
 </template>
 
 <script>
-import MyProduct from './components/MyProduct';
-import List from './components/List.vue';
+// 1.0 样式引入
+import './styles/base.css';
+import './styles/index.css';
+
+import TodoHeader from './components/TodoHeader';
+import TodoMain from './components/TodoMain';
+import TodoFooter from './components/TodoFooter';
+
 export default {
-  data() {
-    return {
-      list: [
-        {
-          id: 1,
-          proname: '超级好吃的棒棒糖',
-          proprice: 18.8,
-          info: '开业大酬宾, 全场8折'
-        },
-        {
-          id: 2,
-          proname: '超级好吃的大鸡腿',
-          proprice: 34.2,
-          info: '好吃不腻, 快来买啊'
-        },
-        {
-          id: 3,
-          proname: '超级无敌的冰激凌',
-          proprice: 14.2,
-          info: '炎热的夏天, 来个冰激凌了'
-        }
-      ]
-    };
-  },
   components: {
-    MyProduct,
-    List
-  },
-  methods: {
-    fn(inde, price) {
-      this.list[inde].proprice > 1 &&
-        (this.list[inde].proprice = (this.list[inde].proprice - price).toFixed(
-          2
-        ));
-    }
+    TodoHeader,
+    TodoMain,
+    TodoFooter
   }
 };
 </script>
-
-<style></style>
