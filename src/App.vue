5ae8c6742858eea243e7ccfe8053832c538bb2fc
ifer-itcast <yangkangkang@itcast.cn>
2021-08-01 14:06:44 +0800

    .

--- src/App.vue
+++ src/App.vue
@@ -1,134 +1,35 @@
 <template>
   <div id="app">
-    <div class="container">
-      <!-- 顶部框模块 -->
-      <div class="form-group">
-        <div class="input-group">
-          <h4>品牌管理</h4>
-        </div>
-      </div>
-
-      <!-- 数据表格 -->
-      <table class="table mt-2 table-bordered table-hover">
-        <thead>
-          <tr>
-            <th>编号</th>
-            <th>资产名称</th>
-            <th>价格</th>
-            <th>创建时间</th>
-            <th>操作</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="obj in list" :key="obj.id">
-            <td>{{ obj.id }}</td>
-            <td>{{ obj.name }}</td>
-
-            <!-- 如果价格超过100，就有red这个类 -->
-            <td :class="{ red: obj.price > 100 }">{{ obj.price }}</td>
-            <td>{{ obj.time | formatDate }}</td>
-            <td><a href="#" @click="delFn(obj.id)">删除</a></td>
-          </tr>
-          <tr v-if="list.length !== 0" style="background-color: #EEE">
-            <td>统计:</td>
-            <td colspan="2">总价钱为: {{ allPrice }}</td>
-            <td colspan="2">平均价: {{ avgPrice }}</td>
-          </tr>
-        </tbody>
-        <tfoot v-if="list.length === 0">
-          <tr>
-            <td colspan="5" style="text-align: center">暂无数据</td>
-          </tr>
-        </tfoot>
-      </table>
-
-      <!-- 添加资产 -->
-      <form class="form-inline">
-        <div class="form-group">
-          <div class="input-group">
-            <input
-              type="text"
-              class="form-control"
-              placeholder="资产名称"
-              v-model="name"
-            />
-          </div>
-        </div>
-        &nbsp;&nbsp;&nbsp;&nbsp;
-        <div class="form-group">
-          <div class="input-group">
-            <input
-              type="text"
-              class="form-control"
-              placeholder="价格"
-              v-model.number="price"
-            />
-          </div>
-        </div>
-        &nbsp;&nbsp;&nbsp;&nbsp;
-        <!-- 阻止表单提交 -->
-        <button class="btn btn-primary" @click.prevent="addFn">添加资产</button>
-      </form>
-    </div>
+    <h3>案例：折叠面板</h3>
+    <panel />
+    <panel />
+    <panel />
   </div>
 </template>
 
 <script>
-import moment from 'moment';
+import Panel from './components/Panel.vue';
 export default {
-  data() {
-    return {
-      name: '', // 名称
-      price: 0, // 价格
-      list: JSON.parse(localStorage.getItem('pList') || '[]')
-    };
-  },
-  methods: {
-    addFn() {
-      if (this.name.trim().length === 0 || this.price === 0) {
-        return alert('不能为空');
-      }
-      const id =
-        this.list.length > 0 ? this.list[this.list.length - 1].id + 1 : 100;
-      this.list.push({
-        id,
-        name: this.name,
-        price: this.price,
-        time: new Date()
-      });
-      this.name = '';
-      this.price = 0;
-    },
-    delFn(id) {
-      const index = this.list.findIndex((obj) => obj.id === id);
-      this.list.splice(index, 1);
-    }
-  },
-  filters: {
-    formatDate(val) {
-      return moment(val).format('YYYY-MM-DD');
-    }
-  },
-  computed: {
-    allPrice() {
-      return this.list.reduce((sum, obj) => (sum += obj.price), 0);
-    },
-    avgPrice() {
-      return (this.allPrice / this.list.length).toFixed(2);
-    }
-  },
-  watch: {
-    list: {
-      handler() {
-        localStorage.setItem('pList', JSON.stringify(this.list));
-      },
-      deep: true
-    }
+  components: {
+    Panel
   }
 };
 </script>
-<style>
-.red {
-  color: red;
+
+<style lang="less">
+body {
+  background-color: #ccc;
+  #app {
+    width: 400px;
+    margin: 20px auto;
+    background-color: #fff;
+    border: 4px solid blueviolet;
+    border-radius: 1em;
+    box-shadow: 3px 3px 3px rgba(0, 0, 0, 0.5);
+    padding: 1em 2em 2em;
+    h3 {
+      text-align: center;
+    }
+  }
 }
 </style>
