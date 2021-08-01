4f07690082f77af89a80b15bba15d33bfd3525a2
ifer-itcast <yangkangkang@itcast.cn>
2021-08-01 16:57:48 +0800

    .

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="todoapp">
     <!-- 除了驼峰, 还可以使用-转换链接 -->
-    <TodoHeader></TodoHeader>
+    <TodoHeader @create="createFn"></TodoHeader>
     <TodoMain :arr="list"></TodoMain>
     <TodoFooter></TodoFooter>
   </section>
@@ -30,6 +30,17 @@ export default {
         { id: 103, name: '打豆豆', isDone: true }
       ]
     };
+  },
+  methods: {
+    createFn(taskName) {
+      const id =
+        this.list.length === 0 ? 100 : this.list[this.list.length - 1].id + 1;
+      this.list.push({
+        id,
+        name: taskName,
+        isDone: false
+      });
+    }
   }
 };
 </script>
--- src/components/TodoHeader.vue
+++ src/components/TodoHeader.vue
@@ -3,10 +3,28 @@
     <h1>todos</h1>
     <input id="toggle-all" class="toggle-all" type="checkbox" />
     <label for="toggle-all"></label>
-    <input class="new-todo" placeholder="输入任务名称-回车确认" autofocus />
+    <input
+      class="new-todo"
+      placeholder="输入任务名称-回车确认"
+      autofocus
+      @keyup.enter="downFn"
+      v-model="task"
+    />
   </header>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      task: ''
+    };
+  },
+  methods: {
+    downFn() {
+      this.$emit('create', this.task);
+      this.task = '';
+    }
+  }
+};
 </script>
