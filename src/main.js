834dc344ece20a9089f7175df7789a2f5dfd78d4
ifer-itcast <yangkangkang@itcast.cn>
2021-08-01 10:29:24 +0800

    .

--- src/main.js
+++ src/main.js
@@ -1,9 +1,8 @@
-import Vue from 'vue';
-import App from './components/App.vue';
+import Vue from 'vue'
+import App from './App.vue'
 
-const vm = new Vue({
-  // 指定 vm 要控制的页面区域
-  el: '#app',
-  // 通过 render 函数，把指定的组件渲染到指定区域中
-  render: h => h(App)
-})
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
