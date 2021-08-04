28ff0438d94010b6f0c7375a917ef1f83a102c29
ifer-itcast <yangkangkang@itcast.cn>
2021-08-04 14:44:44 +0800

    .

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,52 @@
 <template>
-  <div id="container">
-    <div id="app">
-      <h3>案例：折叠面板</h3>
-      <panel>
-        <template v-slot="scope">
-          <p>{{ scope.row.defaultTwo }}</p>
-        </template>
-      </panel>
-    </div>
+  <div>
+    <my-table :arr="list">
+      <template v-slot="scope">
+        <a :href="scope.row.headImgUrl">{{ scope.row.headImgUrl }}</a>
+      </template>
+    </my-table>
+    <my-table :arr="list">
+      <template v-slot="scope">
+        <img
+          :src="scope.row.headImgUrl"
+          style="width:80px; height: 80px;"
+          alt=""
+        />
+      </template>
+    </my-table>
   </div>
 </template>
 
 <script>
-import Panel from './components/Panel.vue';
+import MyTable from './components/MyTable.vue';
 export default {
   name: 'App',
+  data() {
+    return {
+      list: [
+        {
+          name: '小传同学',
+          age: 18,
+          headImgUrl:
+            'http://yun.itheima.com/Upload/./Images/20210303/603f2d2153241.jpg'
+        },
+        {
+          name: '小黑同学',
+          age: 25,
+          headImgUrl:
+            'http://yun.itheima.com/Upload/./Images/20210304/6040b101a18ef.jpg'
+        },
+        {
+          name: '智慧同学',
+          age: 21,
+          headImgUrl:
+            'http://yun.itheima.com/Upload/./Images/20210302/603e0142e535f.jpg'
+        }
+      ]
+    };
+  },
   components: {
-    Panel
+    MyTable
   }
 };
 </script>
-<style scoped>
-#app {
-  width: 400px;
-  margin: 20px auto;
-  background-color: #fff;
-  border: 4px solid blueviolet;
-  border-radius: 1em;
-  box-shadow: 3px 3px 3px rgba(0, 0, 0, 0.5);
-  padding: 1em 2em 2em;
-}
-</style>
