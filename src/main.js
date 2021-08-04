3701c5723f80b4fdc385639229104eccd042be98
ifer-itcast <yangkangkang@itcast.cn>
2021-08-04 19:35:29 +0800

    .

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue';
 import App from './App.vue';
-import { Button } from 'vant';
+import { Button, Form, Field } from 'vant';
 Vue.use(Button);
+Vue.use(Form);
+Vue.use(Field);
 new Vue({
   render: (h) => h(App)
 }).$mount('#app');
