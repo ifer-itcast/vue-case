c8ad943851b9482f556fa442b53a923384483827
ifer-itcast <yangkangkang@itcast.cn>
2021-08-04 15:44:07 +0800

    .

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,39 @@
 <template>
   <div>
     <my-header background="blue" color="white" title="TabBar 案例" />
+    <my-tab-bar :arr="tabList" />
   </div>
 </template>
 
 <script>
 import MyHeader from './components/MyHeader.vue';
+import MyTabBar from './components/MyTabBar.vue';
 export default {
   name: 'App',
   components: {
-    MyHeader
+    MyHeader,
+    MyTabBar
+  },
+  data() {
+    return {
+      tabList: [
+        {
+          iconText: 'icon-shangpinliebiao',
+          text: '商品列表',
+          componentName: 'MyGoodsList'
+        },
+        {
+          iconText: 'icon-sousuo',
+          text: '商品搜索',
+          componentName: 'MyGoodsSearch'
+        },
+        {
+          iconText: 'icon-user',
+          text: '我的信息',
+          componentName: 'MyUserInfo'
+        }
+      ]
+    };
   }
 };
 </script>
---
+++ src/components/MyTabBar.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="my-tab-bar">
+    <div class="tab-item" v-for="(obj, index) in arr" :key="index">
+      <!-- 图标 -->
+      <span :class="obj.iconfont"></span>
+      <!-- 文字 -->
+      <span>{{ obj.text }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyTabBar',
+  props: {
+    arr: {
+      type: Array,
+      required: true,
+      validator(value) {
+        if (value.length >= 2 && value.length <= 5) {
+          return true;
+        } else {
+          console.error('数据必须是 2 ~ 5 项');
+          return false;
+        }
+      }
+    }
+  }
+};
+</script>
+
+<style lang="less" scoped>
+.my-tab-bar {
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: 50px;
+  border-top: 1px solid #ccc;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  background-color: white;
+  .tab-item {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+}
+
+.current {
+  color: #1d7bff;
+}
+</style>
