13441b148bc13c8812f3c970709424557746fbdf
ifer-itcast <yangkangkang@itcast.cn>
2021-08-07 15:10:56 +0800

    .

---
+++ src/views/Layout/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    Layout
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Layout',
+
+  data () {
+    return {
+    }
+  },
+
+  methods: {}
+}
+</script>
+
+<style scoped>
+/* 中间内容区域 - 容器样式(留好上下导航所占位置) */
+.main {
+  padding-top: 46px;
+  padding-bottom: 50px;
+}
+</style>
