cb65b7f9a353e8a849d33d57e15d09bad81c8e19
ifer-itcast <yangkangkang@itcast.cn>
2021-08-07 14:59:18 +0800

    .

--- src/main.js
+++ src/main.js
@@ -1,9 +1,8 @@
 import Vue from 'vue';
 import App from './App.vue';
-import { Button, Form, Field } from 'vant';
-Vue.use(Button);
-Vue.use(Form);
-Vue.use(Field);
+import '@/mobile/flexible';
+import '@/styles/reset.css';
+
 new Vue({
   render: (h) => h(App)
 }).$mount('#app');
